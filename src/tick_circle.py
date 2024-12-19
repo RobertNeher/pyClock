63dcb0079c61fc7c8604ab5101ba448bdfa64ca2
rneher <robert.neher@bartscher-hasenaecker.de>
2024-12-19 22:27:47 +0100

    Backup20241219

--- src/tick_circle.py
+++ src/tick_circle.py
@@ -1,6 +1,6 @@
 import flet as ft
-import math
 import flet.canvas as cv
+import math
 
 def tickCircle(radius: float): #, digitStyle: ft.TextStyle, digitSize: float):
     # startAngle = math.pi
@@ -11,28 +11,18 @@ def tickCircle(radius: float): #, digitStyle: ft.TextStyle, digitSize: float):
     stroke_paint = ft.Paint(color=ft.colors.BLACK26, stroke_width=5, style=ft.PaintingStyle.STROKE)
 
     while digitAngle <= (2.0 * math.pi):
-        # x = radius * (1.0 + math.sin(digitAngle))
-        # y = radius * (1.0 + math.cos(digitAngle))
-
         digitShapes.append(cv.Line(
-            x1=radius,
-            y1=tickSize,
-            x2=radius,
-            y2=0,
-            # x1 = radius * (1.0 + math.sin(digitAngle)),
-            # y1 = tickSize * (1.0 + math.sin(digitAngle)),
-            # x2 = radius * (1.0 + math.sin(digitAngle)),
-            # y2 = radius * (1.0 + math.sin(digitAngle)),
+            # x1 = radius,
+            # y1 = tickSize,
+            # x2 = radius,
+            # y2 = 0,
+            x1 = radius * (1.0 + math.sin(digitAngle)),
+            y1 = tickSize * (1.0 + math.sin(digitAngle)),
+            x2 = radius * (1.0 + math.sin(digitAngle)),
+            y2 = radius * (1.0 + math.sin(digitAngle)),
             paint=stroke_paint
         ))
-        # digitShapes.append(cv.Line(
-        #     x1=radius,
-        #     y1=tickSize,
-        #     x2=radius,
-        #     y2=-radius,
-        #     paint=stroke_paint
-        # ))
 
-    digitAngle += delta
+        digitAngle += delta
 
     return digitShapes
