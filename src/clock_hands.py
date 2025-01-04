ec072fb55080a06dbe2297be824d8e528b7fcd39
rneher <robert.neher@bartscher-hasenaecker.de>
2025-01-04 09:05:44 +0100

    all layers unified including one hand

--- src/clock_hands.py
+++ src/clock_hands.py
@@ -4,45 +4,81 @@ import flet.canvas as cv
 
 from random_color import random_Color
 
-def clockHands(radius:float, settings:dict, randomColor:bool):
-    hands = []
-    minuteHandColor = random_Color() if randomColor else colors["minuteHand"]
-    hourHandColor = random_Color() if randomColor else colors["hourHand"]
-    secondHandColor = random_Color() if randomColor else colors["secondHand"]
-    handsGeometry = settings["hands"]
+def arrowHand(radius: float, translateX: float, translateY: float, settings: dict, randomColor: bool) -> list:
     colors = settings["colors"]
+    handSettings = settings["hands"]
 
-    # timeSecond = datetime.now().second
-    # timeMinute = datetime.now().minute
-    # timeHour = datetime.now().hour
-    hands.append(
-        cv.Path(elements=[
-                cv.Path.LineTo(
-                    0,
-                    hands["secondHandWidth"] / 2.0
-                ),
-                cv.Path.LineTo(
-                    radius * settings["secondHandFactor"] * 0.8,
-                    hands["secondHandWidth"] / 2.0
-                ),
-                cv.Path.LineTo(
-                    radius * settings["secondHandFactor"],
-                    0
-                ),
-                cv.Path.LineTo(
-                    radius * settings["secondHandFactor"] * 0.8,
-                    -hands["secondHandWidth"] / 2.0
-                ),
-                cv.Path.LineTo(
-                    0,
-                    -hands["secondHandWidth"] / 2.0
-                ),
-            ],
-            paint=ft.Paint(
-                style=ft.PaintingStyle.FILL,
-                color=secondHandColor
-            ),    
-        ),
+    secondHandPaintStroke = ft.Paint(
+        style=ft.PaintingStyle.STROKE,
+        stroke_width=2,
+        color=random_Color() if randomColor else colors["secondHand"]
+    )
+    secondHandPaintFill = ft.Paint(
+        style=ft.PaintingStyle.FILL,
+        stroke_width=2,
+        color= random_Color() if randomColor else colors["secondHand"]
     )
 
-    return hands
+    return  [
+            #  -->cv.Circle(translateX, translateY, radius * 0.05, paint=centerDotPaint),
+            cv.Path([
+                cv.Path.MoveTo(
+                        translateX,
+                        translateY
+                ),
+                cv.Path.LineTo(
+                    translateX + 0,
+                    translateY + handSettings["secondHandWidth"] / 2.0
+                ),
+                cv.Path.LineTo(
+                    translateX + radius * handSettings["secondHandFactor"] * 0.9,
+                    translateY + handSettings["secondHandWidth"] / 2.0
+                ),
+                cv.Path.LineTo(
+                    translateX + radius * handSettings["secondHandFactor"],
+                    translateY + 0
+                )
+            ],
+            paint=secondHandPaintStroke
+        ),
+        cv.Path([
+                cv.Path.MoveTo(
+                        translateX,
+                        translateY
+                ),
+                cv.Path.LineTo(
+                    translateX + 0,
+                    translateY - handSettings["secondHandWidth"] / 2.0
+                ),
+                cv.Path.LineTo(
+                    translateX + radius * handSettings["secondHandFactor"] * 0.9,
+                    translateY - handSettings["secondHandWidth"] / 2.0
+                ),
+                cv.Path.LineTo(
+                    translateX + radius * handSettings["secondHandFactor"],
+                    translateY + 0
+                )
+            ],
+            paint=secondHandPaintFill
+        ),
+        #  -->cv.Circle(translateX, translateY, radius * 0.04, paint=centerCirclePaint),
+    ]
+
+
+def clockHands(handType: str, radius:float, settings:dict, randomColor:bool):
+    if handType == "arrow":
+        return arrowHand(
+            radius=radius,
+            translateX=radius,
+            translateY=radius,
+            settings=settings,
+            randomColor=randomColor
+        )
+    # elif handType == "rounded":
+    #     return roundedHand(
+    #         radius=radius,
+    #         translateX=radius,
+    #         translateY=radius,
+    #         settings=settings,
+    #         randomColor=randomColor
+    #     )
--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import keyboard
 import time
 
 import flet as ft
-import flet.canvas as cv
 
 from clock_hands import clockHands
 from random_color import random_Color
@@ -23,39 +22,39 @@ def main(page: ft.Page) -> None:
     period = settings["updatePeriod"]
     radius = settings["radius"]
     colors = settings["colors"]
-    handSettings = settings["hands"]
 
     if face:
         backgroundColor = colors["clockFace"] if not randomColor else random_Color()
     else:
         backgroundColor = ft.Colors.TRANSPARENT
 
-    cp = clockFace(radius=radius, colors=colors, clockFace=face, randomColor=randomColor)
+    clockFaceCanvas = clockFace(radius=radius, colors=colors, clockFace=face, randomColor=randomColor)
 
     if face:
-        cp.shapes.extend(tickCircle(radius=radius, colors=colors, randomColor=randomColor))
-        cp.shapes.extend(digitCircle(radius=radius, colors=colors, randomColor=randomColor))
-        cp.shapes.extend(clockHands(radius=radius, settings=handSettings, colors=colors, randomColor=randomColor))
+        clockFaceCanvas.shapes.extend(tickCircle(radius=radius, colors=colors, randomColor=randomColor))
+        clockFaceCanvas.shapes.extend(digitCircle(radius=radius, colors=colors, randomColor=randomColor))
+        clockFaceCanvas.shapes.extend(clockHands(
+                handType="arrow",
+                radius=radius,
+                randomColor=randomColor,
+                settings=settings
+            )
+        )
 
     page.window.width = 2.2 * radius
     page.window.height = 2.3 * radius
-    # cp.shapes.append(
-    #     clockHands(radius=radius, settings=handSettings, colors=colors, randomColor=randomColor)
-    # )
-    page.add(ft.Container(
-    # clockBase = ft.Container(
-        alignment=ft.alignment.top_center,
-        height = radius * 2,
-        width = radius * 2,
-        bgcolor=backgroundColor,
-        padding=0,
-        content=cp
-    ))
 
-    # while keyboard.is_pressed("q"):
+    page.add(
+        ft.Container(
+            alignment=ft.alignment.top_center,
+            height = radius * 2,
+            width = radius * 2,
+            bgcolor=backgroundColor,
+            padding=0,
+            content=clockFaceCanvas,
+        )
+    )
     page.expand=1
     page.update()
-
-    # time.sleep(period)
 
 ft.app(main)
